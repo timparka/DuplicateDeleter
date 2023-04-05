c2e0711c1b4dfa927bdea129978746b74866ff23
timothy <pimtarka@gmail.com>
2023-04-04 21:23:17 -0400

    finished labby

--- src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/DuplicateDeleter.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/25/18.
  */
@@ -10,6 +12,23 @@ public abstract class DuplicateDeleter<T> implements DuplicateDeleterInterface<T
         this.array = intArray;
     }
 
-    abstract public T[] removeDuplicates(int maxNumberOfDuplications);
-    abstract public T[] removeDuplicatesExactly(int exactNumberOfDuplications);
+    @Override
+    public T[] removeDuplicates(int maxNumberOfDuplications) { //example array [1,2,2,3,3,3] maxNumberOfDupiclications = 2;
+        T[] resultArray = Arrays.stream(array) //streaming it just goes thru each element in array [1,2,2,3,3,3]
+                .filter(item -> Arrays.stream(array) // item = 1 filters all values except [1]
+                        .filter(element -> element.equals(item)) // when item = 2 || 3 number is not added because not true
+                        .count() < maxNumberOfDuplications) //count = 1 maxNumberOfDuplications = 2 statement is true
+                .toArray(size -> Arrays.copyOf(array, size)); //adds to new copy of array
+        return resultArray;
+    }
+
+    @Override
+    public T[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        T[] resultArray = Arrays.stream(array)
+                .filter(item -> Arrays.stream(array)
+                        .filter(element -> element.equals(item))
+                        .count() != exactNumberOfDuplications)
+                .toArray(size -> Arrays.copyOf(array, size));
+        return resultArray;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,24 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        return super.removeDuplicates(maxNumberOfDuplications);
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        return super.removeDuplicatesExactly(exactNumberOfDuplications);
+    }
 }
